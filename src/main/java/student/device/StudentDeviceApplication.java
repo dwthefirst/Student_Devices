21eb81a2bddacc035449024ec786951757827e8a
pandaprogrammer <dwthefirst@gmail.com>
2025-05-29 03:48:43 -0700

    first commit - made entity classes, made database with mysql, made pom file

---
+++ src/main/java/student/device/StudentDeviceApplication.java
@@ -0,0 +1,14 @@
+package student.device;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDeviceApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(StudentDeviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/student/device/entity/DeviceType.java
@@ -0,0 +1,5 @@
+package student.device.entity;
+
+public class DeviceType {
+
+}
